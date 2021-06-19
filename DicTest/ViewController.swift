23737442e093b48ca24f4ff994e96e5729952251
Higgins <Higgins4k@gmail.com>
2021-06-19 16:52:48 -0400

    Initial Commit

---
+++ DicTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DicTest
+//
+//  Created by Christian Higgins on 6/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
