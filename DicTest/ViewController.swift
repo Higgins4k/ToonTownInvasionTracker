fc066a2d3adb3ce5874a7c86f1cc175c97a30da0
Higgins <Higgins4k@gmail.com>
2021-06-20 19:27:43 -0400

    test

--- DicTest/ViewController.swift
+++ DicTest/ViewController.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
+struct InvasionList: Decodable {
+    let type: String
+    let asOf: Int?
+    let progress: String?
+    
+    
+}
+
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let jsonUrlString = "https://www.toontownrewritten.com/api/invasions"
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            
+            guard let data = data else {return }
+            
+            do {
+                let invasions = try JSONDecoder().decode(InvasionList.self, from: data)
+                print(invasions.type)
+            
+            
+            } catch let err {
+                print(err)
+            }
+            
+            
+            }.resume()
+        
     }
 
 
